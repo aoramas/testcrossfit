4a55e21b5db4e67ba55bbac206ae4d1ecbc260fb
santiag0jose0 <santiago.espinosa@bitekso.com>
2023-09-07 15:57:59 -0500

    creacion modelo horario

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,11 @@
 // const { nombreModelo, modeloSchema } = require("./person.model");
 const { User, UserSchema } = require("./User.model");
+const { Horario, HorarioSchema } = require("./Horario.model");
 
 function setupModels(sequelize) {
 //   nombreModelo.init(modeloSchema, nombreModelo.config(sequelize));
   User.init(UserSchema, User.config(sequelize));
+  Horario.init(HorarioSchema, Horario.config(sequelize));
 
 }
 
